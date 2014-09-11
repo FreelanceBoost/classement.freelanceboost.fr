191b1ba8de436fc8d83222957b06d471235f36ed
Xavier Carpentier <contact@xavier-carpentier.com>
2014-09-11 11:40:39 +0200

    feat(design)

--- app/controllers/rockstars_controller.rb
+++ app/controllers/rockstars_controller.rb
@@ -7,8 +7,8 @@ class RockstarsController < ApplicationController
   def create
     
     client = Twitter::REST::Client.new do |config|
-      config.consumer_key        = "1oM199t4yf4Dq8fm2PCnQ"
-      config.consumer_secret     = "CD4X3xIojSjUIw8AdDS8PklnHmZa6w6ZsVwaDNco"
+      config.consumer_key        = "tpUNsQuMvR1Lp3c469loTn6IR"
+      config.consumer_secret     = "x5d898RLATY9skNE0JqOc2sE4TzD0w6hPDcz2RXLhcvZSTgTZr"
       config.access_token        = "92309068-BOTrYCjeogNSzqKTU3Rf0RqW6vq4DwPq9NF6oPJhU"
       config.access_token_secret = "JY4YO81dXLiPBFYWG2rVFm10HBUKr59e08cxT1v2P3FiV"
     end

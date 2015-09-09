afdf8b5f46bcf5dd9f9af98abac4b64741710216
Xavier Carpentier <contact@xavier-carpentier.com>
2015-09-09 16:51:46 +0200

    fix scheduler

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -8,7 +8,7 @@ class RankingController < ApplicationController
           {
             sort: [{ total_followers_count: {order: "desc"}}],
             from: 0,
-            size: 558,
+            size: 542,
             query:
             {
               match_all: { }
